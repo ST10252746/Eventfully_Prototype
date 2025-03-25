330a4312ab90cae000a43ad3ebde143124675e1b
ST10252746 <st10252746@vcconnect.edu.za>
2025-03-25 13:16:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prototype_Eventfully.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
